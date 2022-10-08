d40287fcdf43a8b7629872e69eff0c09a20be610
Mohit1310 <mohitdayama1234@gmail.com>
2022-10-08 21:54:56 +0530

    Adding Forgot password

--- app/src/main/java/com/mohit/foodator/ForgotPwdActivity.java
+++ app/src/main/java/com/mohit/foodator/ForgotPwdActivity.java
@@ -3,12 +3,27 @@ package com.mohit.foodator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ForgotPwdActivity extends AppCompatActivity {
+
+    private EditText emailFP, newPwd, confNewPwd;
+    private Button resetPwd;
+    private TextView loginHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_pwd);
+
+        emailFP = findViewById(R.id.emailReset);
+        newPwd = findViewById(R.id.pwdReset);
+        confNewPwd = findViewById(R.id.confirmPass);
+        resetPwd = findViewById(R.id.resetPwd);
+        loginHere = findViewById(R.id.login_here);
+
+
     }
 }
